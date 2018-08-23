4501dbef3fc9df3c483e42d322d6ec1f11ee720a
Jade Barben <barbenjade>
2018-08-23 15:48:11 -0600

    reuse

---
+++ src/components/searchBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <input placeholder="Search Dailysmarty"/>
+      </div>
+    )
+  }
+}
+export default SearchBar;
