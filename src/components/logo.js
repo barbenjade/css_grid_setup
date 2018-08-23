405d9d7c107214df66a1d1e60d667f89ce5dd6e0
Jade Barben <barbenjade>
2018-08-23 15:09:21 -0600

    corresponding styles

---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="logo-main">
+        <img src="/assets/ds_circle_loop.png"/>
+      </div>
+    )
+  }
+}
+export default Logo;
